ba8def14d515a040647ad5d3634987dd6aad1765
Lineles <106559385+Lineles@users.noreply.github.com>
2022-07-06 13:53:05 +0200

    addet JS

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+
+const user = []
+
+let person1 = {
+    userId: 0001,
+    name: "Hannes",
+    userName: "Hansi",
+    password: "gafsgfe5",
+    age: "19",
+    language: "German", 
+    country: "Germany",
+    subStatus: "Unsubscribt",
+}
+
+let person2 = {
+    userId: 0002,
+    name: "Lisa",
+    userName: "Lissi1",
+    password: "hhga",
+    age: "25",
+    language: "English", 
+    country: "Great Britain",
+    subStatus: "Subscribt",
+}
+
+let person3 = {
+    userId: 0003,
+    name: "Max",
+    userName: "Bob16",
+    password: "ohcackav",
+    age: "14",
+    language: "German", 
+    country: "Austria",
+    subStatus: "Unsubscribt",
+}
+
+user = [person1, person2, person3]
+
+console.log("test");
+console.log(user[1]); 
