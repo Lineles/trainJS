8b412d934bb6dbe6472f339f91bb3e2e89b5563e
Lineles <106559385+Lineles@users.noreply.github.com>
2022-07-09 11:13:25 +0200

    createt a language &sex check

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
+
+// German test 
 // let userNameInput = "Hansi"; 
 // let userPasswortInput = "gafsgfe5";
-
-// let userNameInput = "Bob16"; 
-// let userPasswortInput = "ohcackav";
-
-// let userNameInput = "Lissi1"; 
-// let userPasswortInput = "hhga";
 
 // let userNameInput = "kevkev"; 
 // let userPasswortInput = "544557";
 
-let userNameInput = "Bob16"; 
-let userPasswortInput = "544f557";
+
+// English test 
+// let userNameInput = "Bob16"; 
+// let userPasswortInput = "ohcackav";
+
+let userNameInput = "Lissi1"; 
+let userPasswortInput = "hhga";
+
+
+// Spanish test 
+// let userNameInput = "Melli"; 
+// let userPasswortInput = "gaf656465255";
+
+// let userNameInput = "Jogi"; 
+// let userPasswortInput = "gafddfsddddss5";
+
+
+// // Wrong Passwort
+// let userNameInput = "Bob16"; 
+// let userPasswortInput = "544f557";
+
+// // Wrong Username
+// let userNameInput = "Jopppgi"; 
+// let userPasswortInput = "gafddfsddddss5";
 
 
 
 
+
+//
+// Data Base 
+//
 
 const UserDataBase = [];
 
@@ -28,6 +50,7 @@ let Hansi = {
     language: "German", 
     country: "Germany",
     subStatus: "Unsubscribt",
+    sex: "male"
 }
 UserDataBase.push(Hansi); 
 
@@ -40,6 +63,7 @@ let Lissi1 = {
     language: "English", 
     country: "Great Britain",
     subStatus: "Subscribt",
+    sex: "female",
 }
 UserDataBase.push(Lissi1); 
 
@@ -52,6 +76,7 @@ let Bob16 = {
     language: "English", 
     country: "Great Britain",
     subStatus: "Unsubscribt",
+    sex: "male",
 }
 UserDataBase.push(Bob16); 
 
@@ -64,29 +89,95 @@ let kevkev = {
     language: "German", 
     country: "Austria",
     subStatus: "Subscribt",
+    sex: "male",
 }
 UserDataBase.push(kevkev); 
 
+let Melli = {
+    userId: 0006,
+    name: "Melissa",
+    userName: "Melli",
+    password: "gaf656465255",
+    age: "32",
+    language: "Spanish", 
+    country: "Spain",
+    subStatus: "subscribt",
+    sex: "female",
+}
+UserDataBase.push(Melli); 
 
+let Jogi = {
+    userId: 0005,
+    name: "Jochen",
+    userName: "Jogi",
+    password: "gafddfsddddss5",
+    age: "45",
+    language: "Spanish", 
+    country: "Spain",
+    subStatus: "Unsubscribt",
+    sex: "male",
+}
+UserDataBase.push(Jogi); 
+
+
+
+//
+// Dater Base Check
+//
 for (i = 0; i < UserDataBase.length; i++) {
-    if  (UserDataBase[i].userName === userNameInput) {
-        var logedInUser = UserDataBase[i]; 
-    } 
+    if  (UserDataBase[i].userName !== userNameInput) {
+        // console.log("no match found")
+    } else { var logedInUser = UserDataBase[i]; 
+
+        //
+        // Password check
+        //
+        if (logedInUser.password === userPasswortInput) {
+            checkPassword = true;
+        } else { checkPassword = false }
+
+        if (logedInUser.userName === userNameInput) {
+            checkUsername = true;
+        } else { checkUsername = false }
+
+
+        if (checkUsername && checkPassword) {
+        
+            //
+            // Substatus Check 
+            //
+            if (logedInUser.subStatus === "Subscribt") { 
+                accesEnglish = "You have full acces to  all content!";
+                accesGerman = "Du hast vollen zugriff auf den Content!";
+                accesSpanish = "Tienes acceso completo a todo el contenido!"; 
+            } else { 
+                accesEnglish = "Subscribe to get ful acces to all the content!";
+                accesGerman =  "Subscribe um vollen zugriff auf all den Content zu bekommen!";
+                accesSpanish =  "Suscríbete para tener acceso completo a todo el contenido!"
+             }
+
+            //
+            // Language check & console.log
+            //
+            if (logedInUser.language === "German") {
+                console.log("Guten Tag " + logedInUser.name);
+                console.log(accesGerman);
+            } else if (logedInUser.language === "Spanish") {
+                if (logedInUser.sex === "male") {
+                    console.log("bienvenidos " + logedInUser.name);
+                } else {
+                    console.log("bienvenidas " + logedInUser.name);
+                }
+                console.log(accesSpanish);
+            }  else { 
+                console.log("welcome " + logedInUser.name);
+                console.log(accesEnglish); }
+
+        } else { 
+            if (logedInUser.language === "German") {console.log("Dein Passwort ist falsch, versuche es nocheinmal.")}
+            else {console.log("Your Password is wrong, try again") }   
+        }
+    }
 }
 
-if (logedInUser.password === userPasswortInput) {
-    checkPassword = true;
-} else { checkPassword = false }
 
-if (logedInUser.userName === userNameInput) {
-    checkUsername = true;
-} else { checkUsername = false }
-
-
-if (checkUsername && checkPassword) {
-    if (logedInUser.language === "German") {console.log("Guten Tag " + logedInUser.name)
-    } else {console.log("welcome " + logedInUser.name)}  
-} else { 
-    if (logedInUser.language === "German") {console.log("Dein Username oder Passwort ist falsch, versuche es nocheinmal.")}
-    else {console.log("Your Username or Password is wrong, try again") }   
-}
